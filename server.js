d985540bec209a1f8bafcd3ba426b04837dfcc55
Jerry Ejonavi <jerryasih@gmail.com>
2019-12-04 16:59:29 +0100

    feat: add basic rate limiting

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // Simple Express server for SSR
 
 const express = require('express');
+const RateLimit = require('express-rate-limit');
 const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
+
+const limiter = new RateLimit({
+    windowMs: 60 * 60 * 1000, // 1 hour
+    max: 1000,
+});
+
+app.use(limiter);
+
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
