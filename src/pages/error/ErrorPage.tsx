d2759e2356de04a81d7d0520f7a3783e3cabd434
Jerry Ejonavi <jerryasih@gmail.com>
2020-03-27 02:27:17 +0100

    chore: upgrade to ant@v4 components

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Typography, Button, Icon, Divider } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
+import { Typography, Button, Divider } from 'antd';
 import { Link } from 'react-router-dom';
 import styles from './ErrorPage.module.scss';
 import Helmet from 'react-helmet';
@@ -8,28 +9,26 @@ const { Title } = Typography;
 
 class ErrorPage extends Component {
     render(): JSX.Element {
-        return (
-            <>
-                <Helmet>
-                    <title>Oops! Page not found!</title>
-                </Helmet>
-                <div className={styles.error} style={{ textAlign: 'center' }}>
-                    <Title>
-                        Error                    
-                    </Title>
-                    <p>
-                        Page not found
-                    </p>
-                    <Link to="/">
-                        <Button shape="round" size="large">
-                            Go home
-                            <Divider type="vertical" />
-                            <Icon type="home" />
-                        </Button>
-                    </Link>
-                </div>
-            </>
-        );
+        return <>
+            <Helmet>
+                <title>Oops! Page not found!</title>
+            </Helmet>
+            <div className={styles.error} style={{ textAlign: 'center' }}>
+                <Title>
+                    Error                    
+                </Title>
+                <p>
+                    Page not found
+                </p>
+                <Link to="/">
+                    <Button shape="round" size="large">
+                        Go home
+                        <Divider type="vertical" />
+                        <HomeOutlined />
+                    </Button>
+                </Link>
+            </div>
+        </>;
     }
 }
 
